cb710d867047c3495f2398ff4c3149981df53445
Rafael <rafaelufjf@gmail.com>
2021-03-25 08:53:00 -0300

    exercício4

--- src/codegym/Cat5.java
+++ src/codegym/Cat5.java
@@ -1,9 +1,21 @@
 package codegym;
 
 public class Cat5 {
+	
+	String name;
+	int age;
+	int weight;
+	int strenght;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Cat5 cat = new Cat5();
+		cat.name = "xpto";
+		cat.age = 15;
+		cat.weight = 100;
+		cat.strenght = 10;
+		
+		
 
 	}
 
