15c7f12ba506051468c7541c6b3788819daf4b91
Rafael <rafaelufjf@gmail.com>
2021-03-25 08:52:49 -0300

    exercícios3

---
+++ src/codegym/Cat5.java
@@ -0,0 +1,10 @@
+package codegym;
+
+public class Cat5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/codegym/Dog.java
@@ -0,0 +1,32 @@
+package codegym;
+
+public class Dog {
+
+	String name;
+	int age;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+	public static void main(String[] args) {
+		
+		}
+		
+
+}
+
+
